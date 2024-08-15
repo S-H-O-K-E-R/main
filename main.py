a69d8263c07e9207f3365d9cdae0ffdabdf1c96b
S-H-O-K-E-R <kolyan482650@gmail.com>
2024-08-15 15:44:15 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print('Hellow, woild!')
+
+#1st program
+print(int(9**0.5*5))
+
+#2nd program
+print( 9.99 > 9.98 and 1000 != 1000.1)
+
+#3nd program
+print(2*2+2 == 2*(2+2))
+
+#4nd program
+print(int(123.456*10)%10)
